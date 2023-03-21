8e290ebd3f747797e59bee2367180ea0b8d20a06
Adem Şerif Dur <85637636+Ademseriff@users.noreply.github.com>
2023-03-21 11:17:16 +0300

    arabalisteleme_giriş

--- Controllers/AnaSayfaController.cs
+++ Controllers/AnaSayfaController.cs
@@ -1,13 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApp.Models;
+
 
 namespace WebApp.Controllers
 {
     public class AnaSayfaController : Controller
     {
+        Contex contex;
+        public AnaSayfaController()
+        {
+            contex = new Contex();
+        }
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Index(Arac arac)
+        {
+           var cx= contex.aracs.ToList();
+            
+            for(int i = 0;i<cx.Count;i++)
+            {
+                if (cx[i].AracMarka.Equals(arac.AracMarka))
+                {
+                    return View("AracListele");
+                }
+
+            }
+            return View();
+        }
+
+
     }
 }
