c78b462928c6526a6988f50a5e6d020ed767fcca
Adem Şerif Dur <85637636+Ademseriff@users.noreply.github.com>
2023-03-21 09:55:15 +0300

    Login_GirişPanel

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        Contex contex;
+        public LoginController()
+        {
+
+            contex = new Contex();
+
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Admin admin)
+        {  
+            var cx =  contex.admins.ToList();
+            for (int i = 0; i < cx.Count; i++)
+            {
+                if (cx[i].AdminName.Equals(admin.AdminName))
+                {
+                    return RedirectToAction( "Index", "Home");
+                }
+            }
+
+
+            return View();
+        }
+    }
+}
