f64f6e375110177ef1f511d133dba6eca3b24323
Adem Şerif Dur <85637636+Ademseriff@users.noreply.github.com>
2023-03-27 11:20:26 +0300

    kullanici_login_giriş_sayfası yapıdlı

---
+++ Controllers/KullaniciGirdiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class KullaniciGirdiController : Controller
+    {
+        public IActionResult Index(User user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ Controllers/KullaniciLoginGirisController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class KullaniciLoginGirisController : Controller
+    {
+        Contex contex;
+
+        public KullaniciLoginGirisController() { 
+        
+        contex = new Contex();
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+           var cx = contex.users.ToList();
+
+            for (int i = 0;i<cx.Count;i++)
+            {
+                if (cx[i].KullaniciAd.Equals(user.KullaniciAd) && cx[i].KullaniciSifre.Equals(user.KullaniciSifre))
+                {
+                    return RedirectToAction("Index", "KullaniciGirdi", cx[i]);
+                }
+
+
+
+            }
+
+            return View();
+        }
+
+
+    }
+}
